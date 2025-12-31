15c18f5037b7fcf192bbf75d9d2e1faa7eec9efd
Lucas Andrade <lwksandrade@gmail.com>
2025-12-30 22:25:52 -0300

    Add CORS headers for API

--- src/app.js
+++ src/app.js
@@ -7,6 +7,20 @@ const { swaggerSpec } = require('./swagger');
 
 const app = express();
 
+const corsMiddleware = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+};
+
+app.use(corsMiddleware);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
