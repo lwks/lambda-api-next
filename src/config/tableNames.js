0a82bd56f7040f1c18d77bce8a0615206c94fcc0
lwks_ <lwksandrade@gmail.com>
2026-01-16 22:20:04 -0300

    fix name table

--- src/config/tableNames.js
+++ src/config/tableNames.js
@@ -1,6 +1,6 @@
 const TABLE_NAMES = {
-  candidate: process.env.CANDIDATE_TABLE_NAME || 'Empresas',
-  company: process.env.COMPANY_TABLE_NAME || 'Candidaturas',
+  candidate: process.env.CANDIDATE_TABLE_NAME || 'Candidaturas',
+  company: process.env.COMPANY_TABLE_NAME || 'Empresas',
   user: process.env.USER_TABLE_NAME || 'Usuarios',
   job: process.env.JOB_TABLE_NAME || 'Vagas',
 };
